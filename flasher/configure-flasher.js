0e132d9bf9ec53f527af10902d2cd57ee37e63e5
HarukiToreda <116696711+HarukiToreda@users.noreply.github.com>
2025-02-01 17:05:32 -0500

    Update configure-flasher.js

--- flasher/configure-flasher.js
+++ flasher/configure-flasher.js
@@ -1,12 +1,10 @@
-// Run this any time 
 function updateFlasherConfig() {
     var hardwareMenu = document.getElementById("hardwareMenu");
     var eraseContainer = document.getElementById("eraseContainer");
     var eraseCheckbox = document.getElementById("eraseCheckbox");
     var espWebTools = document.getElementById("espWebTools");
     var downloadLink = document.getElementById("downloadFirmware");
-  
-    // Get the selection from the hardware menu
+
     var nodeHW = hardwareMenu.options[hardwareMenu.selectedIndex].value;
 
     if (nodeHW === "T-Echo") {
@@ -26,4 +24,35 @@ function updateFlasherConfig() {
             espWebTools.manifest = "./firmware/" + nodeHW + "/update.json";
         }
     }
+}
+
+function showDownloadPopup() {
+    // Create a new install dialog element
+    const downloadDialog = document.createElement("ewt-install-dialog");
+
+    // Set the pop-up title and message
+    downloadDialog.heading = "Download Firmware Instructions";
+    downloadDialog.message = "Ensure device DFU mode drive is mounted.<br>Download and copy UF2 file to DFU drive.";
+
+    // Add the button to continue with the download
+    const continueButton = document.createElement("button");
+    continueButton.innerText = "Continue";
+    continueButton.className = "popup-button";
+    continueButton.onclick = function () {
+        window.location.href = document.getElementById("downloadFirmware").href;
+        document.body.removeChild(downloadDialog);
+    };
+
+    // Add close button
+    const closeButton = document.createElement("button");
+    closeButton.innerText = "Close";
+    closeButton.className = "popup-button";
+    closeButton.onclick = function () {
+        document.body.removeChild(downloadDialog);
+    };
+
+    // Append buttons to the dialog
+    downloadDialog.appendChild(continueButton);
+    downloadDialog.appendChild(closeButton);
+    document.body.appendChild(downloadDialog);
 }
