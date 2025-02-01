8d2d4b2bf4ca709d9e7dd162125bf8dcbb208057
HarukiToreda <116696711+HarukiToreda@users.noreply.github.com>
2025-01-31 19:02:39 -0500

    Test

--- node_modules/esptool-js/lib/index.js
+++
@@ -1,5 +0,0 @@
-export { ESPLoader } from "./esploader.js";
-export { ClassicReset, CustomReset, HardReset, UsbJtagSerialReset, validateCustomResetStringSequence, } from "./reset.js";
-export { ROM } from "./targets/rom.js";
-export { Transport } from "./webserial.js";
-export { decodeBase64Data, getStubJsonByChipName } from "./stubFlasher.js";
