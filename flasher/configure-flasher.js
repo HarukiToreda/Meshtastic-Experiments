2cc539a73c60d2b586ba151c472f2e5389d46f61
HarukiToreda <116696711+HarukiToreda@users.noreply.github.com>
2025-02-01 16:29:57 -0500

    Update configure-flasher.js

--- flasher/configure-flasher.js
+++ flasher/configure-flasher.js
@@ -1,18 +1,28 @@
 // Run this any time 
 function updateFlasherConfig() {
-    // Get the menu elements
     var hardwareMenu = document.getElementById("hardwareMenu");
     var eraseCheckbox = document.getElementById("eraseCheckbox");
     var espWebTools = document.getElementById("espWebTools");
+    var downloadLink = document.getElementById("downloadFirmware");
+    var installButton = document.getElementById("installButton");
   
     // Get the selection from the hardware menu
     var nodeHW = hardwareMenu.options[hardwareMenu.selectedIndex].value;
   
     // Decide whether to full-erase, or just update
-    if (eraseCheckbox.checked) {
-      espWebTools.manifest = "./firmware/" + nodeHW + "/install.json"; 
-    }
-    else {
-      espWebTools.manifest = "./firmware/" + nodeHW + "/update.json";
+    if (nodeHW === "T-Echo") {
+        // Hide flashing button and show download link
+        espWebTools.style.display = "none";
+        downloadLink.style.display = "block";
+    } else {
+        // Show flashing button and hide download link
+        espWebTools.style.display = "block";
+        downloadLink.style.display = "none";
+
+        if (eraseCheckbox.checked) {
+            espWebTools.manifest = "./firmware/" + nodeHW + "/install.json";
+        } else {
+            espWebTools.manifest = "./firmware/" + nodeHW + "/update.json";
+        }
     }
 }
